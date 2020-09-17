28477c8197a022da0c1d37858cebf3168e71f0ec
루나 <44334220+TrueToneTeam@users.noreply.github.com>
2020-09-17 20:00:15 +0900

    이벤트 홈페이지 업데이트 및 기타 기능 추가, 오류 수정

--- js/myMenu.js
+++ js/myMenu.js
@@ -1,12 +1,14 @@
 function myMenu1(status){
 	var menu1 = document.getElementById("eventList");
 	var menu2 = document.getElementById("eventListEnd");
+	var menu3 = document.getElementById("eventListSoon");
 	
 	if(status==1){
 		//메뉴 보이기
 		//alert("메뉴 보이기");
 		menu1.style.display = "block";
 		menu2.style.display ="none";
+		menu3.style.display ="none";
 	}
 
 	else{
@@ -14,18 +16,21 @@ function myMenu1(status){
 		//alert("메뉴 감추기");
 		menu1.style.display ="none";
 		menu2.style.display ="none";
+		menu3.style.display ="none";
 	}
 }
 
 function myMenu2(status){
 	var menu1 = document.getElementById("eventList");
 	var menu2 = document.getElementById("eventListEnd");
+	var menu3 = document.getElementById("eventListSoon");
 	
 	if(status==1){
 		//메뉴 보이기
 		//alert("메뉴 보이기");
-		menu2.style.display = "block";
-		menu1.style.display ="none";
+		menu1.style.display = "none";
+		menu2.style.display ="block";
+		menu3.style.display ="none";
 	}
 
 	else{
@@ -33,6 +38,29 @@ function myMenu2(status){
 		//alert("메뉴 감추기");
 		menu1.style.display ="none";
 		menu2.style.display ="none";
+		menu3.style.display ="none";
+	}
+}
+
+function myMenu3(status){
+	var menu1 = document.getElementById("eventList");
+	var menu2 = document.getElementById("eventListEnd");
+	var menu3 = document.getElementById("eventListSoon");
+	
+	if(status==1){
+		//메뉴 보이기
+		//alert("메뉴 보이기");
+		menu1.style.display = "none";
+		menu2.style.display ="none";
+		menu3.style.display ="block";
+	}
+
+	else{
+		//메뉴 감추기
+		//alert("메뉴 감추기");
+		menu1.style.display ="none";
+		menu2.style.display ="none";
+		menu3.style.display ="none";
 	}
 }
 
